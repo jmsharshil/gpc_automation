a476c746cef9d76d31b6e5d6b2a1ddf67e379723
jmsharshil <harshil@jmsadvisory.in>
2025-12-19 10:39:25 +0530

    admin panel added

--- user_auth/admin.py
+++ user_auth/admin.py
@@ -8,3 +8,66 @@
 #         ("MSAL", {"fields": ("msal_oid", "msal_email", "role")}),
 #     )
 #     list_display = ("username", "email", "role", "msal_oid", "is_staff", "is_active")
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    # Fields shown in the user list page
+    list_display = (
+        'username',
+        'email',
+        'role',
+        'is_microsoft_user',
+        'is_staff',
+        'is_active',
+    )
+
+    list_filter = (
+        'role',
+        'is_microsoft_user',
+        'is_staff',
+        'is_active',
+    )
+
+    search_fields = (
+        'username',
+        'email',
+        'microsoft_id',
+    )
+
+    ordering = ('username',)
+
+    # Fields shown on the user detail/edit page
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            'Custom Fields',
+            {
+                'fields': (
+                    'role',
+                    'microsoft_id',
+                    'profile_picture',
+                    'is_microsoft_user',
+                )
+            },
+        ),
+    )
+
+    # Fields shown when creating a new user
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            'Custom Fields',
+            {
+                'fields': (
+                    'role',
+                    'microsoft_id',
+                    'profile_picture',
+                    'is_microsoft_user',
+                )
+            },
+        ),
+    )
